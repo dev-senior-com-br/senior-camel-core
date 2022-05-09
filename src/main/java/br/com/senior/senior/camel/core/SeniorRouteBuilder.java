4bd9c7ece6b6ca6c1ad52ae4615be66010eaccc0
Emerson Kopp <15053421+emersonkopp@users.noreply.github.com>
2022-05-09 10:38:27 -0300

    - Base RouteBuilder. - Logging format.

--- src/main/java/br/com/senior/senior/camel/core/SeniorRouteBuilder.java
+++ src/main/java/br/com/senior/senior/camel/core/SeniorRouteBuilder.java
@@ -22,15 +22,25 @@ public abstract class SeniorRouteBuilder extends RouteBuilder {
 
     private void configureMDC(Exchange exchange) {
         Message message = exchange.getMessage();
-        MDC.put("tenant", message.getHeader("X-Integration-Tenant"));
-        MDC.put("primitive", message.getHeader("X-Integration-Id"));
+
+        Object tenant = message.getHeader("X-Integration-Tenant");
+        Object primitive = message.getHeader("X-Integration-Id");
         Object contextId = message.getHeader("X-Integration-Context-Id");
         if (contextId == null) {
             contextId = UUID.randomUUID();
             message.setHeader("X-Integration-Context-Id", contextId);
         }
+        UUID requestId = UUID.randomUUID();
+
+        MDC.put("tenant", tenant);
+        MDC.put("primitive", primitive);
         MDC.put("contextId", contextId);
-        MDC.put("requestId", UUID.randomUUID());
+        MDC.put("requestId", requestId);
+
+        org.jboss.logmanager.MDC.put("tenant", tenant == null ? null : tenant.toString());
+        org.jboss.logmanager.MDC.put("primitive", primitive == null ? null : primitive.toString());
+        org.jboss.logmanager.MDC.put("contextId", contextId == null ? null : contextId.toString());
+        org.jboss.logmanager.MDC.put("requestId", requestId == null ? null : requestId.toString());
     }
 
 }
